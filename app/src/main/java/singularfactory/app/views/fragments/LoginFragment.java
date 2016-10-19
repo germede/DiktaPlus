cf7aa56b82ab81f4768d16d364c7ca0176094ed8
Gerardo Medina <gerardo.medina103@alu.ulpgc.es>
2016-10-19 14:08:43 +0100

    Basic interface for login

---
+++ app/src/main/java/singularfactory/app/views/fragments/LoginFragment.java
@@ -0,0 +1,38 @@
+package singularfactory.app.views.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Óscar Adae Rodríguez on 18/08/2016.
+ */
+public class LoginFragment extends Fragment {
+
+    public final static String TAG = LoginFragment.class.getSimpleName();
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    /********************/
+    /** PUBLIC METHODS **/
+    /********************/
+
+}
