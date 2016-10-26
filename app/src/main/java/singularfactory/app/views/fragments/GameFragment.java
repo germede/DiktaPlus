c8035dcec2fe97e7a297caf7b23ac02b8a0f6948
Gerardo Medina <gerardo.medina103@alu.ulpgc.es>
2016-10-26 12:57:42 +0100

    She speaks

--- app/src/main/java/singularfactory/app/views/fragments/GameFragment.java
+++ app/src/main/java/singularfactory/app/views/fragments/GameFragment.java
@@ -3,6 +3,8 @@ package singularfactory.app.views.fragments;
 import android.content.Context;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,16 +23,41 @@ import org.json.JSONException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 
 import singularfactory.app.R;
 import singularfactory.app.presenters.PresenterTexts;
 
-public class GameFragment extends BaseFragment {
+public class GameFragment extends BaseFragment implements TextToSpeech.OnInitListener {
     View view;
     TextView pressTheButtonLabel;
     ImageButton playButton;
     EditText gameTextEdit;
     String textToPlay;
+    TextToSpeech tts;
+
+
+    @Override
+    public void onInit(int status) {
+        if (status == TextToSpeech.SUCCESS) {
+            int result = tts.setLanguage(Locale.US);
+            if (result == TextToSpeech.LANG_MISSING_DATA
+                    || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("TTS", "This language is not supported");
+            } else {
+                dictate();
+            }
+        } else {
+            Log.e("TTS", "Initilization failed");
+        }
+    }
+
+    public void dictate () {
+        String [] words = textToPlay.split(" ");
+        for (int i = 0; i < words.length; i++) {
+            
+        }
+    }
 
     public GameFragment() {
         // Required empty public constructor
@@ -44,6 +71,8 @@ public class GameFragment extends BaseFragment {
         pressTheButtonLabel.setVisibility(View.GONE);
         playButton.setImageResource(android.R.drawable.ic_media_pause);
         gameTextEdit.setVisibility(View.VISIBLE);
+
+        tts = new TextToSpeech(getContext(),this);
     }
 
     @Override
@@ -56,6 +85,15 @@ public class GameFragment extends BaseFragment {
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_game, container, false);
         return view;
+    }
+
+    @Override
+    public void onDestroy() {
+        if(tts != null) {
+            tts.stop();
+            tts.shutdown();
+        }
+        super.onDestroy();
     }
 
     public void setTextToPlay (String textToPlay) {
