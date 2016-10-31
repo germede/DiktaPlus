a0cd92b7e89bd3ec39cca2fce0cfe6683186ee91
Gerardo Medina <gera.mdlh@gmail.com>
2016-10-31 17:39:59 +0000

    Settings fragment

--- app/src/main/java/singularfactory/app/views/fragments/SettingsFragment.java
+++ app/src/main/java/singularfactory/app/views/fragments/SettingsFragment.java
@@ -1,23 +1,27 @@
 package singularfactory.app.views.fragments;
 
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.net.Uri;
 import android.os.Bundle;
-import android.preference.PreferenceFragment;
-import android.support.v4.app.Fragment;
+import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import singularfactory.app.R;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
+    Preference deleteAccount;
+
 
     @Override
     public void onCreatePreferences(Bundle bundle, String s) {
         addPreferencesFromResource(R.xml.preferences);
+
+        deleteAccount = getPreferenceManager().findPreference("delete_account");
+        deleteAccount.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                Toast.makeText(getContext(),"hola",Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
     }
 }
